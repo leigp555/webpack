de7e532edb05db86ec51a916de9052374c3ad5f1
leigp <907090585@qq.com>
2021-10-07 03:45:29 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-
-import x from "./x.js"
-console.log(x)
-console.log("hi")
+import css from "./x.css";
+import x from "./x.js";
+console.log(x);
+console.log("hello");
+console.log(css.toString());
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "我是配置中的title",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
