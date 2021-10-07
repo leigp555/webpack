e49c5ad4cc6f04824c487f7de10c842215b027c1
leigp <907090585@qq.com>
2021-10-07 23:08:39 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,14 @@
+yarn build &&
 git add . &&
 git commit -v &&
 git push
+git branch x
+git checkout x
+rm -rf *.json *.js yarn.lock;
+mv dist/* ./
+rm -rf dist;
+git add .
+git commit -m update
+git push
+git checkout main
+
