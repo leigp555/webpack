ca093dfcc8168b18739c2ae3e350ed7cc862413d
leigp <907090585@qq.com>
2021-10-07 23:16:32 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 yarn build &&
 git add . &&
 git commit -v &&
-git push
-git branch x
-git checkout x
-rm -rf *.json *.js yarn.lock;
-mv dist/* ./
-rm -rf dist;
-git add .
-git commit -m update
-git push
+git push &&
+git branch x &&
+git checkout x &&
+rm -rf *.json *.js yarn.lock &&
+mv dist/* ./ &&
+rm -rf dist &&
+git add . &&
+git commit -m update &&
+git push --set-upstream origin x &&
 git checkout main
 
