4c00089bad6178961288b0956c49ad52436d56b1
leigp <907090585@qq.com>
2021-10-07 22:26:30 +0800

    加入懒加载

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import "./y.scss";
 import "./s.less";
 import "./v.styl"
 import sss from"./assets/1.jpg"
-
+console.log(x)
 console.log(sss)
 const gg=document.querySelector(".app")
 gg.innerHTML=`<img src="${sss}">`
+const button =document.createElement("button")
+button.innerText="懒加载"
+
+
+button.onclick=()=>{
+    console.log("你好")
+    const promise =import("./lazy.js")
+    promise.then((module)=>{
+        const fn=module.default
+        fn()
+    },()=>{})
+     
+}
+
+document.querySelector(".button").appendChild(button)
---
+++ src/lazy.js
@@ -0,0 +1,4 @@
+function lazy(){
+    console.log("懒加载模块")
+}
+export default lazy
