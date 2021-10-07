2f79d3c657fcb94635512c67ee86858dda7054b8
leigp <907090585@qq.com>
2021-10-07 19:14:33 +0800

    继承

---
+++ webpack.config.base.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "我是配置中的title",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [],
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,17 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const base = require("./webpack.config.base");
 module.exports = {
+  ...base,
   mode: "development",
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
-  },
   devtool: "inline-source-map",
   devServer: {
     static: "./dist",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "我是配置中的title",
-      template: "src/assets/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
-  ],
   module: {
     rules: [
+      ...base.module.rules,
       {
         test: /\.css$/i,
         //二选一要么css 加载到内存，要么css抽离成文件
